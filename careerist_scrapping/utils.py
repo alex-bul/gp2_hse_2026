585ba5d74721aa87d190e7858afc16cfd01f4943
alex-bul <bualal2005@gmail.com>
2026-04-08 11:59:16 +0300

    add console handler

--- careerist_scrapping/utils.py
+++ careerist_scrapping/utils.py
@@ -11,7 +11,7 @@ html_folder = os.path.join(BASE_DIR, "page_html") # сохраняется html 
 source_urls_file = os.path.join(BASE_DIR, "url_list.txt")  # файл с ссылками на вакансии
 
 result_csv_file = os.path.join(BASE_DIR, "result.csv") # файл с результатами = таблицей с данными
-columns = ["slug", "profession", "salary", "firm_name", "town", "employment", "experience", "description", "date_published", "category"]
+columns = ["slug", "profession", "salary", "firm_name", "city", "employment", "experience", "raw_description", "description", "date_published", "category"]
 
 # создаем если нет файла
 if not result_csv_file in os.listdir(BASE_DIR):
@@ -35,14 +35,18 @@ def create_logger(script_name):
     # мы хотим INFO, так как иначе много логов от селениум набегает
     # уровень логирования (debug, info, warning, error, critical)
     
-    # Создаем handler для записи в файл
+    # Создаем handler для записи в файл и консоль
     file_handler = logging.FileHandler(log_filename, encoding='utf-8')
+    console_handler = logging.StreamHandler()
+
     
     # формат логов: время, уровень, текст
     formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
     file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
     
     # Добавляем handler к логгеру
+    logger.addHandler(console_handler)
     logger.addHandler(file_handler)
     
     return logger
